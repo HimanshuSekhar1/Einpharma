c241f5b5a34c61fd86d0cc97c68988267db538fa
HimanshuSekhar1 <himanshu.sekhar.341@gmail.com>
2022-07-18 02:11:08 +0530

    First Commit

---
+++ What we offer.js
@@ -0,0 +1,47 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animContainer1'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets4.lottiefiles.com/private_files/lf30_cmnwuylv.json'
+})
+
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animContainer2'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets6.lottiefiles.com/packages/lf20_qmrxbp0w.json'
+})
+
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animContainer3'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets4.lottiefiles.com/packages/lf20_fsfbqbph.json'
+})
+
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animContainer4'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets1.lottiefiles.com/packages/lf20_IcG65O.json'
+})
+
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animContainer5'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets10.lottiefiles.com/private_files/lf30_ufgbcgx8.json'
+})
+
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animContainer6'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets3.lottiefiles.com/packages/lf20_2ixzdfvy.json'
+})
---
+++ script code.js
@@ -0,0 +1,56 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animContainer'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets2.lottiefiles.com/packages/lf20_ytlkft45.json' //lotie file path  
+})
+
+
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animContainer1'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets9.lottiefiles.com/packages/lf20_gzntypux.json'
+})
+
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animContainer2'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+path: 'https://assets10.lottiefiles.com/packages/lf20_7v735djr.json'
+})
+
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animContainer3'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+path: 'https://assets4.lottiefiles.com/packages/lf20_8ypuhlx4.json'
+})
+
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animContainer4'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+path: 'https://assets6.lottiefiles.com/packages/lf20_jnw2bd4s.json'
+})
+
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animContainer5'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+path: 'https://assets5.lottiefiles.com/packages/lf20_ngvqungz.json'
+})
+
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animContainer6'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+path: 'https://assets9.lottiefiles.com/packages/lf20_yqhwn4pd.json'
+})
